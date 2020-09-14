db427d968ca425c95d4cb0dde9b9c16e7230ae5e
terrypangws <70142936+terrypangws@users.noreply.github.com>
2020-09-14 17:01:15 +0800

    new

--- jsfiles.js
+++ jsfiles.js
@@ -5,3 +5,28 @@ function message(msg){
 function displayDate(){
     document.getElementById("demo").innerHTML=Date()
 }
+
+function message(msg){
+    document.getElementById('output').innerHTML=msg
+}
+
+function buttonClick(){
+    var answer=200
+    document.getElementById('stuff').innerHTML=answer
+}
+
+function buttonClick2(){
+    var answer=300
+    document.getElementById('stuff').innerHTML=answer
+}
+ function hide(){
+     let x=document.getElementById("img1")
+     x.style.display="none"
+ }
+
+ function unhide(){
+     let x=document.getElementById("img1")
+     let y=document.getElementById('stext')
+     x.style.display="block"
+     y.style.display="block"
+ }
